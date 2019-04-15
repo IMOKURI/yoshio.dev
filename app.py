b38d6a37f66857d0e10b890dca2aaecb781c435b
IMOKURI <nenegi.01mo@gmail.com>
2019-04-15 21:32:45 +0900

    update code

---
+++ app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""
+Source code for https://yoshio.dev
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
