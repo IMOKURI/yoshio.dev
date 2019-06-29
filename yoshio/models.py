d44426840d695550886dcd283d952c1e58fcf9a2
IMOKURI <nenegi.01mo@gmail.com>
2019-06-29 14:07:01 +0900

    update content

--- yoshio/models.py
+++ yoshio/models.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 import flask_login as fl
 import sqlalchemy as sql
-import sqlalchemy.orm as sqlo
-from werkzeug.security import generate_password_hash, check_password_hash
 
 from yoshio import db
 
@@ -12,31 +10,13 @@ class User(db.Model, fl.UserMixin):
 
     lineid = sql.Column(sql.String(128), primary_key=True)
     username = sql.Column(sql.Unicode(128))
-    _password = sql.Column(sql.String(128))
-
-    def _get_password(self):
-        return self._password
-
-    def _set_password(self, password):
-        if password:
-            password = password.strip()
-        self._password = generate_password_hash(password)
-
-    password_descriptor = property(_get_password, _set_password)
-    password = sqlo.synonym('_password', descriptor=password_descriptor)
-
-    def check_password(self, password):
-        password = password.strip()
-        if not password:
-            return False
-        return check_password_hash(self.password, password)
 
     @classmethod
-    def auth(cls, query, lineid, password):
+    def auth(cls, query, lineid):
         user = query(cls).filter(cls.lineid == lineid).first()
         if user is None:
             return None, False
-        return user, user.check_password(password)
+        return user, True
 
     def get_id(self):
         return self.lineid
--- yoshio/working_hours.py
+++ yoshio/working_hours.py
@@ -8,21 +8,12 @@ from flask import (
     render_template,
     request,
 )
-import flask_login as fl
 
 from linebot.exceptions import InvalidSignatureError
 import linebot.models as lm
 
 from yoshio import db, line_bot_api, handler
-from yoshio.models import User
-
-
-import imp
-try:
-    imp.find_module('pysnooper')
-    import pysnooper
-except ImportError:
-    pass
+from yoshio.models import User, WorkingHours
 
 
 bp = Blueprint('working_hours', __name__, url_prefix="/working_hours")
@@ -33,68 +24,17 @@ def index():
     return render_template('pages/wh_index.html')
 
 
-@pysnooper.snoop()
-@bp.route('/login', methods=["GET", "POST"])
-def login():
-    if request.method == 'POST':
-        lineid = request.form['lineid']
-        password = request.form['password']
+@bp.route('/<lineid>')
+def dashboard(lineid):
+    user, authenticated = User.auth(
+        db.session.query,
+        lineid,
+    )
 
-        user, authenticated = User.auth(
-            db.session.query,
-            lineid,
-            password
-        )
+    if authenticated:
+        return render_template('pages/wh_dashboard.html')
 
-        if authenticated:
-            fl.login_user(user, remember=True)
-            flash(u'ログインしました(^^)', 'success')
-            return redirect('/working_hours/dashboard')
-
-        flash(u'ログインに失敗しました。。。', 'fail')
-
-    return render_template('pages/wh_login.html')
-
-
-@bp.route('/dashboard', methods=['GET', 'POST'])
-@fl.login_required
-def dashboard():
-    if request.method == 'POST':
-        # TODO
-        pass
-
-    return render_template('pages/wh_dashboard.html')
-
-
-@bp.route('/register', methods=['GET', 'POST'])
-@fl.login_required
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        lineid = request.form['lineid']
-        password = request.form['password']
-
-        user = User(
-            username=username,
-            lineid=lineid,
-            password=password,
-        )
-
-        # TODO: Error handling
-        db.session.add(user)
-        db.session.commit()
-        flash('Added new user!')
-
-    return render_template('pages/wh_register.html')
-
-
-@bp.route('/logout')
-@fl.login_required
-def logout():
-    user = fl.current_user
-    fl.logout_user()
-    flash(u'ログアウトしました。(^^)ﾉｼ')
-    return redirect('/working_hours')
+    return redirect('/working_hours/index')
 
 
 @bp.route('/callback', methods=['POST'])
@@ -110,20 +50,53 @@ def callback():
 
 @handler.add(lm.FollowEvent)
 def handle_follow(event):
-    uid = event.source.user_id
-    profile = line_bot_api.get_profile(uid)
-    name = profile.display_name
+    lineid = event.source.user_id
+    profile = line_bot_api.get_profile(lineid)
+    username = profile.display_name
 
-    # TODO: Register user to DB
-    pass
+    user = User(
+        username=username,
+        lineid=lineid
+    )
 
+    # TODO: Error handling
+    db.session.add(user)
+    db.session.commit()
+
+    msg = u'下のボタンから勤怠を登録してね。勤怠記録はこちらを参照してね。 https://yoshio.dev/working_hours/{}'.format(lineid)
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        lm.TextSendMessage(text=msg)
+    )
 
 @handler.add(lm.PostbackEvent)
 def handle_postback(event):
-    uid = event.source.user_id
-    action = event.postback.data
+    lineid = event.source.user_id
 
-    msg = event.timestamp
+    if 'begin' in event.postback.data:
+        action = 'begin'
+        msg = u'出勤時刻を記録しました。今日も頑張って！'
+    elif 'end' in event.postback.data:
+        action = 'end'
+        msg = u'退勤時刻を記録しました。お疲れ様でした！'
+    elif 'display' in event.postback.data:
+        action = 'display'
+        msg = u'勤怠記録はこちらを参照してね。 https://yoshio.dev/working_hours/{}'.format(lineid)
+    else:
+        action = None
+        msg = u'おや！？'
+
+    if action in ['bebin', 'end']:
+        wh_data = WorkingHours(
+            lineid=lineid,
+            action=action,
+            date=event.timestamp
+        )
+
+        # TODO: Error handling
+        db.session.add(wh_data)
+        db.session.commit()
 
     line_bot_api.reply_message(
         event.reply_token,
